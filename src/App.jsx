3410bd422a705ebe2195bd49997cea6708a72a38
Aadarsh Manandhar <aadarshmanandhar2@gmail.com>
2026-05-03 22:15:37 +0545

    Imported all necessary codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from './Comp/Header'
 import Footer from './Comp/Footer'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
       <Footer />
     </>
   )
