5c7396bf0d43aa00e98a37bb9b4bed60f8f05b0f
Aadarsh Manandhar <aadarshmanandhar2@gmail.com>
2026-05-03 22:31:04 +0545

    Added icons in header.jsx

--- src/Comp/Header.jsx
+++ src/Comp/Header.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import { RiGlobalFill } from "react-icons/ri";
+import { FaFacebookF } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+
+
+
 
 function Header() {
     return (
         <>
-            <section className='bg-[#1A1A2E] text-white py-2'>
+            <section className='bg-[#1A1A2E] text-white py-2 text-sm'>
                 <div className="container mx-auto flex justify-between">
                     <p>वि सं २०८३ बैशाख २० आइतवार</p>
-                    <ul className='flex gap-3'>
-                        <li>English</li>
-                        <li></li>
+                    <ul className='flex gap-3 items-center'>
+                        <li className='flex items-center'> <RiGlobalFill /> English</li>
+                        <li> | </li>
+                        <li><FaFacebookF /></li>
+                        <li><FaYoutube /></li>
                     </ul>
                 </div>
             </section>
