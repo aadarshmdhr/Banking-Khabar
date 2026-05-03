0277b5949be415760cdd0436d2f015a95fae1c9f
Aadarsh Manandhar <aadarshmanandhar2@gmail.com>
2026-05-03 22:23:05 +0545

    Added background color and other details in header.jsx

--- src/Comp/Header.jsx
+++ src/Comp/Header.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 function Header() {
     return (
-        <div>Header</div>
+        <>
+            <section className='bg-[#1A1A2E] text-white py-2'>
+                <div className="container mx-auto flex justify-between">
+                    <p>वि सं २०८३ बैशाख २० आइतवार</p>
+                    <ul className='flex gap-3'>
+                        <li>English</li>
+                        <li></li>
+                    </ul>
+                </div>
+            </section>
+        </>
     )
 }
 
